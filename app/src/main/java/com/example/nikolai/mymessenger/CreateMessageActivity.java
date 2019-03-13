6af73d5e9798b3279e7186c287a1e5655cec9152
Nikolay Veselov <veselov2344@mail.ru>
2019-03-13 17:30:51 +0300

    first

---
+++ app/src/main/java/com/example/nikolai/mymessenger/CreateMessageActivity.java
@@ -0,0 +1,23 @@
+package com.example.nikolai.mymessenger;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+    public void onSendMessage(View view) {
+        EditText messageView = (EditText)findViewById(R.id.WriteMessage);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/nikolai/mymessenger/ReceiveMessageActivity.java
@@ -0,0 +1,41 @@
+package com.example.nikolai.mymessenger;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ReceiveMessageActivity extends Activity {
+
+    public static String EXTRA_MESSAGE = "message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+        Intent intent = getIntent();
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = (TextView)findViewById(R.id.message);
+        messageView.setText(messageText);
+    }
+    public void SendToEmailComponent(View view) {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"Test Recipient"});
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Test subject");
+
+        emailIntent.putExtra(Intent.EXTRA_TEXT, EXTRA_MESSAGE);
+
+        Intent chooser = Intent.createChooser(emailIntent, "Send message via...");
+
+        if (emailIntent.resolveActivity(getPackageManager()) != null) {
+            Toast.makeText(ReceiveMessageActivity.this, "Opening mailing app", Toast.LENGTH_SHORT).show();
+            startActivity(chooser);
+        } else {
+            Toast.makeText(ReceiveMessageActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
